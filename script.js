fba89bcbd74cddcedba71430b08d2d8559957045
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-20 23:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let para = document.querySelector("#status");
+let btn = document.querySelector("#enterBtn");
+
+btn.addEventListener("click", () => {
+    let head = document.createElement("h1");
+    head.textContent = "Entered Metaverse";
+
+    if (para) {
+        para.replaceWith(head); // Replaces <p> with <h1>
+    }
+});
